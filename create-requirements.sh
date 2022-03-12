5c100bc45a0638ef68a201a61ebd202101c2b24d
dlsh <dlsh3858@gmail.com>
2022-03-12 20:26:44 +0000

    change c-r

--- create-requirements.sh
+++ create-requirements.sh
@@ -4,4 +4,4 @@ if [[ -z "$VIRTUAL_ENV" ]]; then
     source /home/is3107/is3107/bin/activate 
 fi
 
-pip freeze > /home/darren/requirements.txt
+pip freeze | grep --invert-match pkg_resources > requirements.txt
